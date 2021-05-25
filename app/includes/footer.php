4da64a998f9199127b72b85769f75e99c88cc6a0
Sahil Patel <sp0715007@gmail.com>
2021-05-25 12:25:54 -0400

    final-commit

---
+++ app/includes/footer.php
@@ -0,0 +1,63 @@
+ <!-- footer -->
+ <div class="footer">
+    <div class="footer-content">
+
+      <div class="footer-section about">
+        <h1 class="logo-text"><span>Sahil</span>Patel</h1>
+        <p>
+        This site is dedicated to analyzing and discussing various topics on Football in order to foster a better sense of community and greater understanding about the beautiful game we all love so much.
+        </p>
+        <div class="contact">
+          <span><i class="fas fa-phone"></i> &nbsp; 437-982-2407</span>
+          <span><i class="fas fa-envelope"></i> &nbsp; info@sahilcode.com</span>
+        </div>
+        <div class="socials">
+          <a href="https://www.facebook.com/sahil.sakhiya" target="_blank"><i class="fab fa-facebook"></i></a>
+          <a href="https://www.instagram.com/sahil.code/" target="_blank"><i class="fab fa-instagram"></i></a>
+          <a href="https://github.com/sahilsakhiya11" target="_blank"><i class="fab fa-github"></i></a>
+          <a href="https://www.youtube.com/channel/UCOAZsre-SJ3WUUeqpBevNTw" target="_blank"><i class="fab fa-youtube"></i></a>
+        </div>
+      </div>
+
+      <div class="footer-section links">
+        <h2>Quick Links</h2>
+        <br>
+        <ul>
+          <a href="https://www.instagram.com/sahil.code/" target="_blank">
+            <li>Instagram</li>
+          </a>
+          <a href="https://github.com/sahilsakhiya11" target="_blank">
+            <li>Github</li>
+          </a>
+          <a href="https://www.youtube.com/channel/UCOAZsre-SJ3WUUeqpBevNTw" target="_blank">
+            <li>Yotube</li>
+          </a>
+          <a href="https://sahil-patel-react-portfolio.netlify.app" target="_blank">
+            <li>Portfolio</li>
+          </a>
+          <a href="https://www.facebook.com/sahil.sakhiya" target="_blank">
+            <li>Facebook</li>
+          </a>
+        </ul>
+      </div>
+
+      <div class="footer-section contact-form">
+        <h2>Contact me</h2>
+        <br>
+        <form action="index.html" method="post">
+          <input type="email" name="email" class="text-input contact-input" placeholder="Your email address...">
+          <textarea rows="4" name="message" class="text-input contact-input" placeholder="Your message..."></textarea>
+          <button type="submit" class="btn btn-big contact-btn">
+            <i class="fas fa-envelope"></i>
+            Send
+          </button>
+        </form>
+      </div>
+
+    </div>
+
+    <div class="footer-bottom">
+      &copy; sahilcode.com | Designed by Sahil Patel
+    </div>
+  </div>
+  <!-- // footer -->
